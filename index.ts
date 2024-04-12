27d1d054389fa2b557c6cdb1d1eaa9d8b363ff83
Muhammad Sarim Khan <sarimimran882@gmail.com>
2024-04-13 00:28:52 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+const currency:any = {
+    USD: 1,
+    PKR: 277.83,
+    GBP: 0.80,
+    SAR: 3.75,
+    EUR: 0.94,
+};
+
+let convertor = await inquirer.prompt([
+    {name: "from",
+    message: "Enter from currency: ", 
+    type: "list", 
+    choices: ["USD", "PKR", "GBP", "SAR", "EUR"]},
+    
+    {name: "to",
+    message: "Enter to currency: ", 
+    type: "list", 
+    choices: ["USD", "PKR", "GBP", "SAR", "EUR"]},
+
+    {name: "amount",
+    message: "Enter your Amount: ",
+    type: "number"
+    }
+]);
+
+let fromAmount = currency[convertor.from];
+let toAmount = currency[convertor.to];
+let baseamount = convertor.amount / fromAmount;
+let finalAmount = baseamount * toAmount;
+
+console.log(chalk.green.bold(`Your converted amount is ${finalAmount.toFixed(2)}`));
